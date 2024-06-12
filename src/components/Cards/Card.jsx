0390d949a549e4e20f770fb06e26a9905a981d24
Dario Lo Forti <lofortidario@gmail.com>
2024-06-12 15:30:12 +0200

    update card title to Italian language

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -14,7 +14,7 @@ const Card = () => {
       <figure>
         <img src="https://kinsta.com/it/wp-content/uploads/sites/2/2023/04/react-must-be-in-scope-when-using-jsx.jpg" alt="React" />
         <figcaption className={cardStyle.figcaption}>
-          <h2 className={cardStyle.cardTitle}>Card Title</h2>
+          <h2 className={cardStyle.cardTitle}>Titolo del post</h2>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus voluptates fuga sit modi dicta. Animi nihil perspiciatis id atque nostrum velit culpa voluptas dolore iste.</p>
         </figcaption>
       </figure>
