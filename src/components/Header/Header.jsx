c81aafba3f4616480f173f3314639987dcc6200c
Dario Lo Forti <lofortidario@gmail.com>
2024-06-12 15:56:53 +0200

    update header title styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import headerStyle from './Header.module.css';
 const Header = () => {
   return (
     <header className={headerStyle.header}>
-      <h1>Il mio blog</h1>
+      <h1 className={headerStyle.title}>Il mio blog</h1>
     </header>
   );
 }
