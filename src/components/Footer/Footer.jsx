02ef40232c0aeaa3954e0c243edf835cf010b86a
Dario Lo Forti <lofortidario@gmail.com>
2024-06-12 14:15:23 +0200

    Refactor App component and add Header and Footer components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error modi accusantium pariatur cupiditate adipisci, repellat id sit vel iure amet iusto veritatis magni asperiores earum, possimus consectetur unde ipsam ab.</p>
+    </footer>
+  );
+};
+
+export default Footer;
